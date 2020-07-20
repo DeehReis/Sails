58f0bc7e3d7bb4c1f143a75bb76c6fc9696bbfeb
DeehReis <49690290+DeehReis@users.noreply.github.com>
2020-07-20 17:37:44 -0300

    Update saidas_detalhes.php

--- paginas_adm/saidas_detalhes.php
+++ paginas_adm/saidas_detalhes.php
@@ -76,6 +76,30 @@
 				</div>
 			</div>
 		</div>
+		
+		<!-- Modal de exclusão -->
+
+		<div class="modal fade" id="modalConfirma" tabindex="-1" role="dialog">
+			<div class="modal-dialog" role="document">
+		    	<div class="modal-content">
+		      		<div class="modal-header">
+		        		<h5 class="modal-title" id="exampleModalLabel" style="padding-left: 5%">&bull; Confirmar exclusão</h5>
+
+		        		<button type="button" class="close" data-dismiss="modal">
+		          			<span>&times;</span>
+		        		</button>
+
+		      		</div>
+		      		<div class="modal-body" style="text-align: center">
+
+		      			<a class="btn btn-primary excluir" href="../paginas_processos/editar.php?id=<?php echo $id; ?>&pg=saidas&delete=<?php echo true; ?>">
+		   					Excluir
+		   				</a>
+
+					</div>
+				</div>
+			</div>
+		</div>
 
 		<!-- Chamada do header -->
 
@@ -183,9 +207,9 @@
 				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalModelo" style="margin-right: 20px">
 	  				Editar dados da saída
 				</button>
-				<a class="btn btn-primary excluir" href="../paginas_processos/editar.php?id=<?php echo $id; ?>&pg=saidas&delete=<?php echo true; ?>">
-		   			Excluir
-		   		</a>
+				<button type="button" class="btn btn-primary excluir" data-toggle="modal" data-target="#modalConfirma" style="margin-right: 20px">
+	  				Excluir
+				</button>
 			</div>	
 		</div>
 
@@ -196,9 +220,9 @@
 				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalModelo" style="margin-right: 20px">
 	  				Editar dados da saída
 				</button>
-				<a class="btn btn-primary excluir" href="../paginas_processos/editar.php?id=<?php echo $id; ?>&pg=saidas&delete=<?php echo true; ?>">
-		   			Excluir
-		   		</a>
+				<button type="button" class="btn btn-primary excluir" data-toggle="modal" data-target="#modalConfirma" style="margin-right: 20px">
+	  				Excluir
+				</button>
 		   		<br><br>
 			</div>	
 		</div>
