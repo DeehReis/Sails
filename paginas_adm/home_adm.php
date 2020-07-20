08ba3c9c0635e70fd41755ea65d571cfc99fbeb5
DeehReis <49690290+DeehReis@users.noreply.github.com>
2020-07-20 17:40:15 -0300

    Update home_adm.php

--- paginas_adm/home_adm.php
+++ paginas_adm/home_adm.php
@@ -99,14 +99,14 @@
 
 			<div>
 				<div class="notificacao rounded" style="background-color: #80bfff; float: left; margin-left: 70px;">
-					Total de livros cadastrados : <span style="font-weight: bold"><?php echo count(itens::all()); ?></span>
+					Livros cadastrados : <span style="font-weight: bold"><?php echo count(itens::all()); ?></span>
 					<hr>
-					Total disponíveis no estoque : <span style="font-weight: bold"><?php echo count(itens::where(['_status'=>1])->get()); ?></span>
+					Livros disponíveis no estoque : <span style="font-weight: bold"><?php echo count(itens::where(['_status'=>1])->get()); ?></span>
 				</div>
 				<div class="notificacao rounded" style="background-color: #80bfff; float: right; margin-right: 70px;">
-					Total atualmente com alunos : <span style="font-weight: bold"><?php echo count(itens::where(['_status'=>0])->get()); ?></span>
+					Livros atualmente com alunos : <span style="font-weight: bold"><?php echo count(itens::where(['_status'=>0])->get()); ?></span>
 					<hr>
-					Total com devolução atrasada : <span style="font-weight: bold"><?php echo count(saidas::where(['_status'=>0])->where('dias_atraso','>',0)->get()); ?></span>
+					Livros com devolução atrasada : <span style="font-weight: bold"><?php echo count(saidas::where(['_status'=>0])->where('dias_atraso','>',0)->get()); ?></span>
 				</div>
 				<br style="clear: left;">
 			</div>
@@ -153,13 +153,13 @@
 				<!-- Dados úteis -->
 
 				<div style="text-align: center">
-				<div style="width: 65%;display: inline-block; background-color: #80bfff;">Total de livros cadastrados</div>
+				<div style="width: 65%;display: inline-block; background-color: #80bfff;">Livros cadastrados</div>
 				<div style="width: 33%;display: inline-block; background-color: white;"><span style="font-weight: bold"><?php echo count(itens::all()); ?></span></div>
-				<div style="width: 65%;display: inline-block; background-color: #80bfff;">Total disponíveis no estoque</div>
+				<div style="width: 65%;display: inline-block; background-color: #80bfff;">Livros disponíveis no estoque</div>
 				<div style="width: 33%;display: inline-block; background-color: white;"><span style="font-weight: bold"><?php echo count(itens::where(['_status'=>1])->get()); ?></span></div>
-				<div style="width: 65%;display: inline-block; background-color: #80bfff;">Total atualmente com alunos</div>
+				<div style="width: 65%;display: inline-block; background-color: #80bfff;">Livros atualmente com alunos</div>
 				<div style="width: 33%;display: inline-block; background-color: white;"><span style="font-weight: bold"><?php echo count(itens::where(['_status'=>0])->get()); ?></span></div>
-				<div style="width: 65%;display: inline-block; background-color: #80bfff;">Total com devolução atrasada</div>
+				<div style="width: 65%;display: inline-block; background-color: #80bfff;">Livros com devolução atrasada</div>
 				<div style="width: 33%;display: inline-block; background-color: white;"><span style="font-weight: bold"><?php echo count(saidas::where(['_status'=>0])->where('dias_atraso','>',0)->get()); ?></span></div>
 				</div>
 			</div>		
